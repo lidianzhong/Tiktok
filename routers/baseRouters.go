cf85a1407026622ecf251f290f601fa9929a524e
lidianzhong <2942033733@qq.com>
2023-08-29 18:10:11 +0800

    Fix the processing of the token parameter in the feed interface.

--- routers/baseRouters.go
+++ routers/baseRouters.go
@@ -9,7 +9,7 @@ import (
 func BaseRoutersInit(r *gin.Engine) {
 	baseRouters := r.Group("/douyin")
 	{
-		baseRouters.GET("/feed", middleware.JWTMiddleWare(), controller.Feed)
+		baseRouters.GET("/feed", controller.Feed)
 		baseRouters.POST("/publish/action/", middleware.JWTMiddleWare(), controller.PublishVideoController)
 		baseRouters.POST("/user/register", controller.Register)
 		baseRouters.POST("/user/login", controller.Login)
